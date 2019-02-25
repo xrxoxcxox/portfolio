6a22098ea479e7d3f862dd2e30c88d13f2440bbe
xrxoxcxox <xrxoxcxox@gmail.com>
2019-02-26 03:07:14 +0900

    要素にクラス追加 refs #1

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,15 +2,17 @@ import React from "react";
 
 import SEO from "../components/seo";
 
+import styles from "./index.module.css";
+
 const IndexPage = () => (
-  <>
+  <div className={styles.container}>
     <SEO title="WiP WIP" />
-    <h1>
+    <h1 className={styles.title}>
       Wip
       <br />
       WIP
     </h1>
-  </>
+  </div>
 );
 
 export default IndexPage;
