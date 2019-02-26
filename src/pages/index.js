a3de8407d7486402ae9fe9b97accdfc5a2991016
xrxoxcxox <xrxoxcxox@gmail.com>
2019-02-27 00:45:48 +0900

    アニメーション追加

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,26 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import SEO from "../components/seo";
 
 import "typeface-montserrat";
+
+const contrast = keyframes`
+  0% {
+    filter: blur(0) contrast(1);
+  }
+  10% {
+    filter: blur(0) contrast(1);
+  }
+  60% {
+    filter: blur(0.8rem) contrast(1);
+  }
+  90% {
+    filter: blur(4rem) contrast(2);
+  }
+  100% {
+    filter: blur(0) contrast(1);
+  }
+`;
 
 const Container = styled.div`
   display: flex;
@@ -20,6 +38,11 @@ const Title = styled.h1`
   font-size: calc(2rem + 20vw);
   line-height: 0.85;
   color: ${props => (props.lowercase ? "#c7c7cc" : "#8e8e93")};
+  animation: ${contrast} ${props => (props.lowercase ? "6000ms" : "6400ms")}
+    ease-in-out infinite normal;
+  &::selection {
+    background: none;
+  }
 `;
 
 const IndexPage = () => (
