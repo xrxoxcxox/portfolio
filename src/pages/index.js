c14d6facadc56c35b0392eee8f68a0dee2bebf19
xrxoxcxox <xrxoxcxox@gmail.com>
2019-02-27 00:09:29 +0900

    styled-component適用

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,33 @@
 import React from "react";
-
+import styled from "styled-components";
 import SEO from "../components/seo";
 
 import "typeface-montserrat";
-import styles from "./index.module.css";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100vh;
+`;
+
+const Title = styled.h1`
+  display: block;
+  font-family: montserrat, sans-serif;
+  font-weight: 900;
+  font-size: calc(2rem + 20vw);
+  line-height: 0.85;
+  color: ${props => (props.lowercase ? "#c7c7cc" : "#8e8e93")};
+`;
 
 const IndexPage = () => (
-  <div className={styles.container}>
+  <Container>
     <SEO title="WiP WIP" />
-    <h1 className={styles.title}>
-      Wip
-      <br />
-      WIP
-    </h1>
-  </div>
+    <Title lowercase>WiP</Title>
+    <Title>WIP</Title>
+  </Container>
 );
 
 export default IndexPage;
