960fe467445b839a650c47c3af34cfcece43a8ee
xrxoxcxox <xrxoxcxox@gmail.com>
2019-03-03 22:31:55 +0900

    ひとまずadobe fontsを使えるようには refs #5

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,16 +26,7 @@ module.exports = {
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
       }
-    },
-    `gatsby-plugin-styled-components`,
-    {
-      resolve: "gatsby-plugin-web-font-loader",
-      options: {
-        typekit: {
-          id: "wyq3rwo"
-        }
-      }
-    }
+    }`gatsby-plugin-styled-components`
 
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,7 @@
 import React from "react";
-import styled, { keyframes } from "styled-components";
+import Helmet from "react-helmet";
+import styled, { keyframes, createGlobalStyle } from "styled-components";
 import SEO from "../components/seo";
-
-import "typeface-montserrat";
 
 const contrast = keyframes`
   0% {
@@ -22,6 +21,15 @@ const contrast = keyframes`
   }
 `;
 
+const GlobalStyles = createGlobalStyle`
+  html {
+    visibility: hidden;
+  }
+  html.wf-active {
+    visibility: visible;
+  }
+`;
+
 const Container = styled.div`
   display: flex;
   flex-direction: column;
@@ -33,7 +41,7 @@ const Container = styled.div`
 
 const Title = styled.h1`
   display: block;
-  font-family: montserrat, sans-serif;
+  font-family: proxima-nova, sans-serif;
   font-weight: 900;
   font-size: calc(2rem + 20vw);
   line-height: 0.85;
@@ -46,11 +54,28 @@ const Title = styled.h1`
 `;
 
 const IndexPage = () => (
-  <Container>
-    <SEO title="WiP WIP" />
-    <Title lowercase>WiP</Title>
-    <Title>WIP</Title>
-  </Container>
+  <>
+    <GlobalStyles />
+    <Helmet>
+      <script>
+        {`
+          (function(d) {
+            var config = {
+              kitId: 'wyq3rwo',
+              scriptTimeout: 3000,
+              async: true
+            },
+            h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+          })(document);
+        `}
+      </script>
+    </Helmet>
+    <Container>
+      <SEO title="WiP WIP" />
+      <Title lowercase>WiP</Title>
+      <Title>WIP</Title>
+    </Container>
+  </>
 );
 
 export default IndexPage;
