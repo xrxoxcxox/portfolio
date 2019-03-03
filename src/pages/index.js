7954890615c6ccee3c33210c44471e1feefb127b
xrxoxcxox <xrxoxcxox@gmail.com>
2019-03-04 02:27:04 +0900

    文字のスタイル切り替えとアニメーション実装 refs #7

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,11 +8,8 @@ const contrast = keyframes`
   0% {
     filter: blur(0) contrast(1);
   }
-  10% {
-    filter: blur(0) contrast(1);
-  }
   60% {
-    filter: blur(0.8rem) contrast(1);
+    filter: blur(0) contrast(1);
   }
   90% {
     filter: blur(4rem) contrast(2);
@@ -24,28 +21,48 @@ const contrast = keyframes`
 
 const Container = styled.div`
   display: flex;
-  justify-content: center;
   align-items: center;
-  width: 100%;
+  width: 103.2rem;
+  padding: 0 2.4rem;
   height: 100vh;
+  margin: auto;
 `;
 
 const Title = styled.h1`
   font-family: proxima-nova, sans-serif;
   font-weight: 900;
-  font-size: calc(2rem + 8vw);
+  font-size: calc(2rem + 2vw);
   line-height: 1;
-  margin-left: ${props => (props.WiP ? "calc(0.4rem + 2vw)" : 0)};
-  color: ${props => (props.WiP ? colors.Blue : colors.Black)};
-  animation: ${contrast} ${props => (props.WiP ? "6000ms" : "0")} ease-in-out
-    infinite normal;
+  margin-left: ${props => (props.WiP ? "calc(0.4rem + 0.8vw)" : 0)};
+  animation: ${contrast} ${props => (props.WiP ? "2000ms" : "0")} ease-in-out
+    normal;
 `;
 
-const IndexPage = () => (
-  <>
-    <Helmet>
-      <script>
-        {`
+const WiP = styled.span`
+  color: ${colors.Blue};
+`;
+
+export default class IndexPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      atanuk: "",
+      space: "",
+      ortfolio: ""
+    };
+    this.componentDidMount = this.componentDidMount.bind(this);
+  }
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ atanuk: "atanuk", space: " ", ortfolio: "ortfolio" });
+    }, 1825);
+  }
+  render() {
+    return (
+      <>
+        <Helmet>
+          <script>
+            {`
           (function(d) {
             var config = {
               kitId: 'wyq3rwo',
@@ -55,14 +72,21 @@ const IndexPage = () => (
             h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
           })(document);
         `}
-      </script>
-    </Helmet>
-    <Container>
-      <SEO title="Keisuke WiP" />
-      <Title>Keisuke</Title>
-      <Title WiP>WiP</Title>
-    </Container>
-  </>
-);
-
-export default IndexPage;
+          </script>
+        </Helmet>
+        <Container>
+          <SEO title="Keisuke WiP" />
+          <Title>Keisuke</Title>
+          <Title WiP>
+            <WiP>W</WiP>
+            {this.state.atanuk}
+            <WiP>i</WiP>
+            {this.state.space}
+            <WiP>P</WiP>
+            {this.state.ortfolio}
+          </Title>
+        </Container>
+      </>
+    );
+  }
+}
