6199667aa500836a80033fbef0dfe88950939d61
xrxoxcxox <xrxoxcxox@gmail.com>
2019-03-04 02:59:50 +0900

    サブタイトルまで含めてのスタイルとアニメーションへ refs #7

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import styled, { keyframes } from "styled-components";
 import colors from "../styles/colors.js";
 import SEO from "../components/seo";
 
-const contrast = keyframes`
+const title = keyframes`
   0% {
     filter: blur(0) contrast(1);
   }
@@ -19,9 +19,25 @@ const contrast = keyframes`
   }
 `;
 
+const subTitle = keyframes`
+  0% {
+    opacity: 0;
+  }
+  60% {
+    opacity: 0;
+  }
+  90% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+`;
+
 const Container = styled.div`
   display: flex;
-  align-items: center;
+  flex-direction: column;
+  justify-content: center;
   width: 103.2rem;
   padding: 0 2.4rem;
   height: 100vh;
@@ -29,17 +45,28 @@ const Container = styled.div`
 `;
 
 const Title = styled.h1`
+  display: inline-block;
   font-family: proxima-nova, sans-serif;
   font-weight: 900;
   font-size: calc(2rem + 2vw);
-  line-height: 1;
+  font-feature-settings: "salt";
+  letter-spacing: 0.02em;
   margin-left: ${props => (props.WiP ? "calc(0.4rem + 0.8vw)" : 0)};
-  animation: ${contrast} ${props => (props.WiP ? "2000ms" : "0")} ease-in-out
+  animation: ${title} ${props => (props.WiP ? "4000ms" : "0")} ease-in-out
     normal;
 `;
 
 const WiP = styled.span`
   color: ${colors.Blue};
+`;
+
+const SubTitle = styled.h2`
+  font-family: source-han-sans-japanese, sans-serif;
+  font-weight: 400;
+  font-size: calc(0.8rem + 0.8vw);
+  font-feature-settings: "palt";
+  letter-spacing: 0.02em;
+  animation: ${subTitle} 4000ms ease-in-out normal;
 `;
 
 export default class IndexPage extends React.Component {
@@ -55,7 +82,7 @@ export default class IndexPage extends React.Component {
   componentDidMount() {
     setTimeout(() => {
       this.setState({ atanuk: "atanuk", space: " ", ortfolio: "ortfolio" });
-    }, 1825);
+    }, 3700);
   }
   render() {
     return (
@@ -75,16 +102,19 @@ export default class IndexPage extends React.Component {
           </script>
         </Helmet>
         <Container>
-          <SEO title="Keisuke WiP" />
-          <Title>Keisuke</Title>
-          <Title WiP>
-            <WiP>W</WiP>
-            {this.state.atanuk}
-            <WiP>i</WiP>
-            {this.state.space}
-            <WiP>P</WiP>
-            {this.state.ortfolio}
-          </Title>
+          <SEO title="Keisuke Watanuki Portfolio" />
+          <div>
+            <Title>Keisuke</Title>
+            <Title WiP>
+              <WiP>W</WiP>
+              {this.state.atanuk}
+              <WiP>i</WiP>
+              {this.state.space}
+              <WiP>P</WiP>
+              {this.state.ortfolio}
+            </Title>
+          </div>
+          <SubTitle>頑張って早く公開します。絶賛製作中。</SubTitle>
         </Container>
       </>
     );
--- src/styles/colors.js
+++ src/styles/colors.js
@@ -1,4 +1,5 @@
 export default {
   Black: "#000",
+  Gray: "#8e8e93",
   Blue: "#007aff"
 };
