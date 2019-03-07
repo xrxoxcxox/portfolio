6ba27a7d73ae4ed0c11952a3ed1ee4f216d88bff
xrxoxcxox <xrxoxcxox@gmail.com>
2019-03-08 03:28:33 +0900

    画像表示できるように refs #9

--- src/components/image.js
+++ src/components/image.js
@@ -1,32 +1,37 @@
-import React from "react"
-import { StaticQuery, graphql } from "gatsby"
-import Img from "gatsby-image"
+import React from "react";
+import { StaticQuery, graphql } from "gatsby";
+import Img from "gatsby-image";
 
-/*
- * This component is built using `gatsby-image` to automatically serve optimized
- * images with lazy loading and reduced file sizes. The image is loaded using a
- * `StaticQuery`, which allows us to load the image from directly within this
- * component, rather than having to pass the image data down from pages.
- *
- * For more information, see the docs:
- * - `gatsby-image`: https://gatsby.app/gatsby-image
- * - `StaticQuery`: https://gatsby.app/staticquery
- */
-
-const Image = () => (
+const Image = props => (
   <StaticQuery
     query={graphql`
       query {
-        placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
-          childImageSharp {
-            fluid(maxWidth: 300) {
-              ...GatsbyImageSharpFluid
+        images: allFile {
+          edges {
+            node {
+              relativePath
+              name
+              childImageSharp {
+                sizes(maxWidth: 1080) {
+                  ...GatsbyImageSharpSizes
+                }
+              }
             }
           }
         }
       }
     `}
-    render={data => <Img fluid={data.placeholderImage.childImageSharp.fluid} />}
+    render={data => {
+      const image = data.images.edges.find(n => {
+        return n.node.relativePath.includes(props.filename);
+      });
+      if (!image) {
+        return null;
+      }
+
+      const imageSizes = image.node.childImageSharp.sizes;
+      return <Img alt={props.alt} sizes={imageSizes} />;
+    }}
   />
-)
-export default Image
+);
+export default Image;
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,35 @@ import SEO from "../components/seo";
 import Layout from "../components/layout";
 import TopTitle from "../components/topTitle";
 
+import aboutPortfolio from "../images/aboutportfolio.jpg";
+import aboutMe from "../images/aboutme.jpg";
+
+import styled from "styled-components";
+
+const ImageContainer = styled.div`
+  grid-column: 1 / 7;
+`;
+
+const AboutPortfolio = styled.div`
+  background-image: url(${aboutPortfolio});
+  background-size: contain;
+  grid-column: 1 / 7;
+`;
+
+const AboutMe = styled.div`
+  background-image: url(${aboutMe});
+  background-size: contain;
+  grid-column: 7 / 13;
+`;
+
 export default () => (
   <>
     <AdobeFonts />
     <SEO title="Keisuke Watanuki Portfolio" />
     <Layout>
       <TopTitle />
+      <AboutPortfolio />
+      <AboutMe />
     </Layout>
   </>
 );
