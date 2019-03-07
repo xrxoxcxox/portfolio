393090996543232ca9c1878ba935c6a127e8743f
xrxoxcxox <xrxoxcxox@gmail.com>
2019-03-08 01:09:06 +0900

    タイトルアニメーションをcomponentへ refs #9

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,86 +1,9 @@
 import React from "react";
 import Helmet from "react-helmet";
-import styled, { keyframes } from "styled-components";
-import colors from "../styles/colors.js";
 import SEO from "../components/seo";
-
-const title = keyframes`
-  0% {
-    filter: blur(0) contrast(1);
-  }
-  60% {
-    filter: blur(0) contrast(1);
-  }
-  90% {
-    filter: blur(4rem) contrast(2);
-  }
-  100% {
-    filter: blur(0) contrast(1);
-  }
-`;
-
-const subTitle = keyframes`
-  0% {
-    opacity: 0;
-  }
-  95% {
-    opacity: 0;
-  }
-  100% {
-    opacity: 1;
-  }
-`;
-
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  width: 103.2rem;
-  padding: 0 2.4rem;
-  height: 100vh;
-  margin: auto;
-`;
-
-const Title = styled.h1`
-  display: inline-block;
-  font-family: proxima-nova, sans-serif;
-  font-weight: 900;
-  font-size: calc(2rem + 2vw);
-  font-feature-settings: "salt";
-  letter-spacing: 0.02em;
-  margin-left: ${props => (props.WiP ? "calc(0.4rem + 0.8vw)" : 0)};
-  animation: ${title} ${props => (props.WiP ? "4000ms" : "0")} ease-in-out
-    normal;
-`;
-
-const WiP = styled.span`
-  color: ${colors.Blue};
-`;
-
-const SubTitle = styled.h2`
-  font-family: source-han-sans-japanese, sans-serif;
-  font-weight: 400;
-  font-size: calc(0.8rem + 0.8vw);
-  font-feature-settings: "palt";
-  letter-spacing: 0.02em;
-  animation: ${subTitle} 4000ms ease-in-out normal;
-`;
+import TopTitle from "../components/topTitle";
 
 export default class IndexPage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      atanuk: "",
-      space: "",
-      ortfolio: ""
-    };
-    this.componentDidMount = this.componentDidMount.bind(this);
-  }
-  componentDidMount() {
-    setTimeout(() => {
-      this.setState({ atanuk: "atanuk", space: " ", ortfolio: "ortfolio" });
-    }, 3700);
-  }
   render() {
     return (
       <>
@@ -98,21 +21,8 @@ export default class IndexPage extends React.Component {
         `}
           </script>
         </Helmet>
-        <Container>
-          <SEO title="Keisuke Watanuki Portfolio" />
-          <div>
-            <Title>Keisuke</Title>
-            <Title WiP>
-              <WiP>W</WiP>
-              {this.state.atanuk}
-              <WiP>i</WiP>
-              {this.state.space}
-              <WiP>P</WiP>
-              {this.state.ortfolio}
-            </Title>
-          </div>
-          <SubTitle>頑張って早く公開します。絶賛製作中。</SubTitle>
-        </Container>
+        <SEO title="Keisuke Watanuki Portfolio" />
+        <TopTitle />
       </>
     );
   }
