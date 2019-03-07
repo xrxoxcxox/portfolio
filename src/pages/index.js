a2b8eefb8394d901715878edc02c22a9d95127c1
xrxoxcxox <xrxoxcxox@gmail.com>
2019-03-08 01:21:53 +0900

    フォント呼び出しを別コンポーネントに refs #9

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Helmet from "react-helmet";
+import AdobeFonts from "../components/adobeFonts";
 import SEO from "../components/seo";
 import TopTitle from "../components/topTitle";
 
@@ -7,20 +7,7 @@ export default class IndexPage extends React.Component {
   render() {
     return (
       <>
-        <Helmet>
-          <script>
-            {`
-          (function(d) {
-            var config = {
-              kitId: 'wyq3rwo',
-              scriptTimeout: 3000,
-              async: true
-            },
-            h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
-          })(document);
-        `}
-          </script>
-        </Helmet>
+        <AdobeFonts />
         <SEO title="Keisuke Watanuki Portfolio" />
         <TopTitle />
       </>
