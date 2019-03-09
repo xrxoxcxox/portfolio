5f90fad9e4a6528e8c9b8e33b1879c13d039b563
xrxoxcxox <xrxoxcxox@gmail.com>
2019-03-10 04:22:22 +0900

    作品詳細へのサムネイル変更 refs #13

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,20 +3,13 @@ import SEO from "../components/seo";
 import Layout from "../components/layout";
 import TopTitle from "../components/topTitle";
 
-import aboutPortfolio from "../images/aboutportfolio.jpg";
+import aboutThisPortfolio from "../images/aboutportfolio.jpg";
 import aboutMe from "../images/aboutme.jpg";
 
 import styled from "styled-components";
 
-const AboutPortfolio = styled.div`
-  background-image: url(${aboutPortfolio});
-  background-size: contain;
-  grid-column: span 6;
-  height: 37.8rem;
-`;
-
-const AboutMe = styled.div`
-  background-image: url(${aboutMe});
+const Work = styled.div`
+  background-image: url(${props => props.title});
   background-size: contain;
   grid-column: span 6;
   height: 37.8rem;
@@ -26,7 +19,7 @@ export default () => (
   <Layout>
     <SEO title="Keisuke Watanuki Portfolio" />
     <TopTitle />
-    <AboutPortfolio />
-    <AboutMe />
+    <Work title={aboutThisPortfolio} />
+    <Work title={aboutMe} />
   </Layout>
 );
