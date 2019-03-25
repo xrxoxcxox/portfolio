14ad1043ffe9d806993435abd907d287b742a34d
xrxoxcxox <xrxoxcxox@gmail.com>
2019-03-26 01:30:59 +0900

    styled-componentsとの競合を解消 refs #21

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
     author: `@xrxoxcxox`
   },
   plugins: [
+    `gatsby-plugin-emotion`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
@@ -25,13 +26,6 @@ module.exports = {
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
-      }
-    },
-    `gatsby-plugin-styled-components`,
-    {
-      resolve: `gatsby-plugin-emotion`,
-      options: {
-        // Accepts all options defined by `babel-plugin-emotion` plugin.
       }
     }
 
