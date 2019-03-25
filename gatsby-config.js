0ef2141e7745a4dc28fe7d914f6f79c0f054f8ff
xrxoxcxox <xrxoxcxox@gmail.com>
2019-03-26 01:09:46 +0900

    emotion導入 refs #21

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,7 +27,13 @@ module.exports = {
         icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
       }
     },
-    `gatsby-plugin-styled-components`
+    `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-plugin-emotion`,
+      options: {
+        // Accepts all options defined by `babel-plugin-emotion` plugin.
+      }
+    }
 
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
