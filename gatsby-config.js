65a0c925ce5e1e04ef3195e9c9094a9ae751cc6f
xrxoxcxox <xrxoxcxox@gmail.com>
2019-09-28 01:03:28 +0900

    [#87]gatsby-transformer-remark導入

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,6 +16,14 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `release-notes`,
+        path: `${__dirname}/src/release-notes`,
+      },
+    },
+    `gatsby-transformer-remark`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
