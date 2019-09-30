9843912ae73b04a685e5edc0efc3370603a83397
xrxoxcxox <xrxoxcxox@gmail.com>
2019-09-30 23:39:35 +0900

    [#83]新しい定数の定義

--- src/styles/Typography.js
+++ src/styles/Typography.js
@@ -9,6 +9,48 @@ const Title = css`
   }
 `
 
+const Headline1 = css`
+  font-size: 4rem;
+  line-height: 4.8rem;
+  @media (max-width: 768px) {
+    font-size: 3rem;
+    line-height: 3.8rem;
+  }
+`
+
+const Headline2 = css`
+  font-size: 2.4rem;
+  line-height: 3.2rem;
+  @media (max-width: 768px) {
+    font-size: 2rem;
+    line-height: 3rem;
+  }
+`
+
+const Headline3 = css`
+  font-size: 2rem;
+  line-height: 2.8rem;
+  @media (max-width: 768px) {
+    font-size: 1.8rem;
+    line-height: 2.8rem;
+  }
+`
+
+const Body1 = css`
+  font-size: 1.6rem;
+  line-height: 2.8rem;
+`
+
+const Body2 = css`
+  font-size: 1.4rem;
+  line-height: 2rem;
+`
+
+const Body3 = css`
+  font-size: 1.2rem;
+  line-height: 1.8rem;
+`
+
 const Headline = css`
   font-size: 4rem;
   line-height: 4.8rem;
@@ -49,6 +91,12 @@ const Caption = css`
 
 export default {
   Title: Title,
+  Headline1: Headline1,
+  Headline2: Headline2,
+  Headline3: Headline3,
+  Body1: Body1,
+  Body2: Body2,
+  Body3: Body3,
   Headline: Headline,
   Subhead: Subhead,
   Body: Body,
