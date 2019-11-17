6f6ce490c3362da55571d272558b5beb93d51c3a
xrxoxcxox <xrxoxcxox@gmail.com>
2019-11-17 15:05:31 +0900

    [#93]graphqlをインポート

--- src/components/LinksToTheWork.js
+++ src/components/LinksToTheWork.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link, StaticQuery } from 'gatsby'
+import { Link, StaticQuery, graphql } from 'gatsby'
 import Img from 'gatsby-image'
 
 import { css } from '@emotion/core'
