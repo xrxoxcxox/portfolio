283c0e613a61762174c3404158c1902e5d2cfc04
xrxoxcxox <watanuki@ju-zen.com>
2020-04-05 04:15:10 +0900

    [#130]Fix loop

--- src/components/FigmaGraph.js
+++ src/components/FigmaGraph.js
@@ -2,36 +2,105 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
 import { css } from '@emotion/core'
-import Color from '../styles/Color'
-import Size from '../styles/Size'
-import Typography from '../styles/Typography'
+// import Color from '../styles/Color'
+// import Size from '../styles/Size'
+// import Typography from '../styles/Typography'
 
 const root = css`
-  grid-column: 1 / -1;
+  grid-column: span 6;
 `
 
 export default () => {
-  const [data, setData] = useState([])
-  const FIGMA_ENDPOINT =
-    'https://api.figma.com/v1/teams/681529201922078422/projects'
+  const [versions, setVerions] = useState([])
+  const FIGMA_VERSIONS_ENDPOINT =
+    'https://api.figma.com/v1/files/S4BeZk9p2DI5C42gSpIM1l/versions'
   useEffect(() => {
     axios
-      .get(FIGMA_ENDPOINT, {
+      .get(FIGMA_VERSIONS_ENDPOINT, {
         headers: {
           'X-FIGMA-TOKEN': '40392-21150d59-5fb8-4040-95ef-76e5c2100b59',
         },
       })
       .then((response) => {
-        setData(response.data.projects)
+        setVerions(response.data.versions)
+        console.log(response.data.versions)
       })
   }, [])
+
+  const now = new Date()
+  const days = [...Array(100)].map((_, i) =>
+    new Intl.DateTimeFormat('sv-SE', {
+      dateStyle: 'short',
+    }).format(now - i * 86400000)
+  )
   return (
     <>
       <ul css={root}>
-        {data.map((item) => (
-          <li key={item.id}>{item.name}</li>
+        {days.map((day) => (
+          <li key={day}>{day}:</li>
+        ))}
+      </ul>
+      <ul css={root}>
+        {versions.map((version) => (
+          <li key={version.id}>{version.created_at}</li>
         ))}
       </ul>
     </>
   )
 }
+
+// const FileList = (id) => {
+//   const [file, setFile] = useState([])
+//   const FIGMA_PROJECTS_ENDPOINT = 'https://api.figma.com/v1/projects'
+//   useEffect(() => {
+//     axios
+//       .get(`${FIGMA_PROJECTS_ENDPOINT}/${id.id}/files`, {
+//         headers: {
+//           'X-FIGMA-TOKEN': '40392-21150d59-5fb8-4040-95ef-76e5c2100b59',
+//         },
+//       })
+//       .then((response) => {
+//         setFile(response.data.files)
+//       })
+//   }, [])
+//   return (
+//     <>
+//       <ul key={id.id}>
+//         {file.map((item) => (
+//           <>
+//             <li key={item.key}>{item.name}</li>
+//           </>
+//         ))}
+//       </ul>
+//     </>
+//   )
+// }
+
+// export default () => {
+//   const [project, setProject] = useState([])
+//   const FIGMA_TEAMS_ENDPOINT =
+//     'https://api.figma.com/v1/teams/681529201922078422/projects'
+//   useEffect(() => {
+//     axios
+//       .get(FIGMA_TEAMS_ENDPOINT, {
+//         headers: {
+//           'X-FIGMA-TOKEN': '40392-21150d59-5fb8-4040-95ef-76e5c2100b59',
+//         },
+//       })
+//       .then((response) => {
+//         setProject(response.data.projects)
+//       })
+//   }, [])
+//   return (
+//     <>
+//       <ul css={root}>
+//         {project.map((item) => (
+//           <>
+//             <li key={item.id}>{item.name}</li>
+//             <FileList id={item.id} />
+//           </>
+//         ))}
+//       </ul>
+//     </>
+//   )
+// }
