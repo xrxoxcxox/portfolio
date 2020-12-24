eae2f9aa3dfdbfe1d0e7462d7b382cc382071c51
xrxoxcxox <watanuki@ju-zen.com>
2020-12-25 03:07:39 +0900

    [#178]Apply react-helmet-async to src/components/Seo.js

--- src/components/Seo.js
+++ src/components/Seo.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import Helmet from 'react-helmet'
+import { Helmet } from 'react-helmet-async'
 import { useStaticQuery, graphql } from 'gatsby'
 
 import image from '../images/OGP.png'
 
-export const Seo = ({ description, title, ogImagePath }) => {
+export const Seo = ({ title, description, ogImagePath }) => {
   const data = useStaticQuery(graphql`
     {
       site {
@@ -21,54 +21,19 @@ export const Seo = ({ description, title, ogImagePath }) => {
   const metaTitle = title || data.site.siteMetadata.title
   const ogImage = ogImagePath ? `${data.site.siteMetadata.siteUrl}${ogImagePath}` : `${data.site.siteMetadata.siteUrl}${image}`
   return (
-    <Helmet
-      htmlAttributes={{
-        lang: `ja`,
-      }}
-      title={title ? `${title} | ${data.site.siteMetadata.title}` : data.site.siteMetadata.title}
-      meta={[
-        {
-          name: `description`,
-          content: metaDescription,
-        },
-        {
-          property: `og:title`,
-          content: metaTitle,
-        },
-        {
-          property: `og:url`,
-          content: data.site.siteMetadata.siteUrl,
-        },
-        {
-          property: `og:description`,
-          content: metaDescription,
-        },
-        {
-          property: `og:type`,
-          content: `website`,
-        },
-        {
-          name: `og:image`,
-          content: ogImage,
-        },
-        {
-          name: `twitter:card`,
-          content: `summary_large_image`,
-        },
-        {
-          name: `twitter:site`,
-          content: data.site.siteMetadata.author,
-        },
-        {
-          name: `twitter:title`,
-          content: metaTitle,
-        },
-        {
-          name: `twitter:description`,
-          content: metaDescription,
-        },
-      ]}
-    >
+    <Helmet>
+      <html lang='ja' />
+      <title>{title ? `${title} | ${data.site.siteMetadata.title}` : data.site.siteMetadata.title}</title>
+      <meta name='description' content={metaDescription} />
+      <meta property='og:title' content={metaTitle} />
+      <meta property='og:url' content={data.site.siteMetadata.siteUrl} />
+      <meta property='og:description' content={metaDescription} />
+      <meta property='og:type' content='website' />
+      <meta property='og:image' content={ogImage} />
+      <meta property='twitter:card' content='summary_large_image' />
+      <meta property='twitter:site' content={data.site.siteMetadata.author} />
+      <meta property='twitter:title' content={metaTitle} />
+      <meta property='twitter:description' content={metaDescription} />
       <link rel='icon' href='/icon.svg' type='image/svg+xml' />
     </Helmet>
   )
