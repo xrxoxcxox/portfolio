74dc1f1c0756ae38a664c54ae01a9f199b56316f
Keisuke Watanuki <watanuki@ju-zen.com>
2023-10-01 23:27:11 +0900

    Implement with reference to the contents of https://www.patreon.com/posts/2022-stream-1-62805056

---
+++ src/effects/CustomShader.js
@@ -0,0 +1,67 @@
+import { Vector2 } from "three";
+
+/**
+ * Dot screen shader
+ * based on glfx.js sepia shader
+ * https://github.com/evanw/glfx.js
+ */
+
+const DotScreenShader = {
+  uniforms: {
+    tDiffuse: { value: null },
+    tSize: { value: new Vector2(256, 256) },
+    center: { value: new Vector2(0.5, 0.5) },
+    angle: { value: 1.57 },
+    scale: { value: 1.0 },
+  },
+
+  vertexShader: /* glsl */ `
+		varying vec2 vUv;
+
+		void main() {
+			vUv = uv;
+			gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+		}`,
+
+  fragmentShader: /* glsl */ `
+		uniform vec2 center;
+		uniform float angle;
+		uniform float scale;
+		uniform vec2 tSize;
+
+		uniform sampler2D tDiffuse;
+
+		varying vec2 vUv;
+
+		float pattern() {
+			float s = sin( angle ), c = cos( angle );
+
+			vec2 tex = vUv * tSize - center;
+			vec2 point = vec2( c * tex.x - s * tex.y, s * tex.x + c * tex.y ) * scale;
+
+			return ( sin( point.x ) * sin( point.y ) ) * 4.0;
+		}
+
+
+    float random(vec2 p) {
+      vec2 k1 = vec2(
+        23.14069263277926, // e^pi (Gelfond's constant)
+        2.665144142690225 // 2^sqrt(2) (Gelfond–Schneider constant)
+      );
+      return fract(
+        cos(dot(p, k1)) * 12345.6789
+      );
+    }
+
+
+
+		void main() {
+			vec4 color = texture2D( tDiffuse, vUv );
+        vec2 uvrandom = vUv;
+        uvrandom.y *= random(vec2(uvrandom.y, 0.4));
+        color.rgb +=random(uvrandom)*0.1;
+        gl_FragColor = color;
+		}`,
+};
+
+export { DotScreenShader };
---
+++ src/effects/granyGradients.js
@@ -0,0 +1,175 @@
+import * as THREE from "three";
+
+import fragment from "./shader/fragment.glsl?raw";
+import fragment1 from "./shader/fragment1.glsl?raw";
+import vertex from "./shader/vertex.glsl?raw";
+import vertex1 from "./shader/vertex1.glsl?raw";
+
+import { DotScreenShader } from "./CustomShader";
+import { EffectComposer } from "three/examples/jsm/postprocessing/EffectComposer.js";
+import { RenderPass } from "three/examples/jsm/postprocessing/RenderPass.js";
+import { ShaderPass } from "three/examples/jsm/postprocessing/ShaderPass.js";
+
+export default class Sketch {
+  constructor(options) {
+    this.scene = new THREE.Scene();
+
+    this.container = options.dom;
+    this.width = this.container.offsetWidth;
+    this.height = this.container.offsetHeight;
+    this.renderer = new THREE.WebGLRenderer();
+    this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+    this.renderer.setSize(this.width, this.height);
+    this.renderer.setClearColor(0xeeeeee, 1);
+    this.renderer.physicallyCorrectLights = true;
+    this.renderer.outputEncoding = THREE.sRGBEncoding;
+
+    this.container.appendChild(this.renderer.domElement);
+
+    this.camera = new THREE.PerspectiveCamera(
+      70,
+      window.innerWidth / window.innerHeight,
+      0.001,
+      1000
+    );
+
+    this.camera.position.set(0, 0, 1.3);
+    this.time = 0;
+
+    this.isPlaying = true;
+
+    this.addObjects();
+    this.initPost();
+    this.resize();
+    this.render();
+    this.setupResize();
+    this.settings();
+  }
+
+  settings() {
+    let that = this;
+    this.settings = {
+      progress: 0,
+      mRefractionRatio: 1.02,
+      mFresnelBias: 0.1,
+      mFresnelScale: 4,
+      mFresnelPower: 2,
+    };
+  }
+
+  initPost() {
+    this.composer = new EffectComposer(this.renderer);
+    this.composer.addPass(new RenderPass(this.scene, this.camera));
+
+    const effect1 = new ShaderPass(DotScreenShader);
+    effect1.uniforms["scale"].value = 4;
+    this.composer.addPass(effect1);
+  }
+
+  setupResize() {
+    window.addEventListener("resize", this.resize.bind(this));
+  }
+
+  resize() {
+    this.width = this.container.offsetWidth;
+    this.height = this.container.offsetHeight;
+    this.renderer.setSize(this.width, this.height);
+    this.composer.setSize(this.width, this.height);
+    this.camera.aspect = this.width / this.height;
+
+    // image cover
+    this.imageAspect = 853 / 1280;
+    let a1;
+    let a2;
+    if (this.height / this.width > this.imageAspect) {
+      a1 = (this.width / this.height) * this.imageAspect;
+      a2 = 1;
+    } else {
+      a1 = 1;
+      a2 = this.height / this.width / this.imageAspect;
+    }
+
+    this.camera.updateProjectionMatrix();
+  }
+
+  addObjects() {
+    this.cubeRenderTarget = new THREE.WebGLCubeRenderTarget(256, {
+      format: THREE.RGBAFormat,
+      generateMipmaps: true,
+      minFilter: THREE.LinearMipMapLinearFilter,
+      encoding: THREE.sRGBEncoding,
+    });
+
+    this.cubeCamera = new THREE.CubeCamera(0.1, 10, this.cubeRenderTarget);
+
+    let that = this;
+    this.material = new THREE.ShaderMaterial({
+      extensions: {
+        derivatives: "#extension GL_OES_standard_derivatives : enable",
+      },
+      side: THREE.DoubleSide,
+      uniforms: {
+        time: { value: 0 },
+        resolution: { value: new THREE.Vector4() },
+      },
+      // wireframe: true,
+      // transparent: true,
+      vertexShader: vertex,
+      fragmentShader: fragment,
+    });
+
+    this.geometry = new THREE.SphereGeometry(1.5, 32, 32);
+
+    this.plane = new THREE.Mesh(this.geometry, this.material);
+    this.scene.add(this.plane);
+
+    let geo = new THREE.SphereGeometry(0.4, 32, 32);
+    this.mat = new THREE.ShaderMaterial({
+      extensions: {
+        derivatives: "#extension GL_OES_standard_derivatives : enable",
+      },
+      side: THREE.DoubleSide,
+      uniforms: {
+        time: { value: 0 },
+        tCube: { value: 0 },
+        mRefractionRatio: { value: 1.02 },
+        mFresnelBias: { value: 0.1 },
+        mFresnelScale: { value: 4 },
+        mFresnelPower: { value: 2 },
+        resolution: { value: new THREE.Vector4() },
+      },
+      vertexShader: vertex1,
+      fragmentShader: fragment1,
+    });
+
+    this.smallSphere = new THREE.Mesh(geo, this.mat);
+    this.scene.add(this.smallSphere);
+  }
+
+  stop() {
+    this.isPlaying = false;
+  }
+
+  play() {
+    if (!this.isPlaying) {
+      this.isPlaying = true;
+      this.render();
+    }
+  }
+
+  render() {
+    if (!this.isPlaying) return;
+    this.time += 0.01;
+    this.smallSphere.visible = false;
+    this.cubeCamera.update(this.renderer, this.scene);
+    this.smallSphere.visible = true;
+    this.mat.uniforms.tCube.value = this.cubeRenderTarget.texture;
+    this.material.uniforms.time.value = this.time;
+    requestAnimationFrame(this.render.bind(this));
+    this.composer.render(this.scene, this.camera);
+  }
+}
+
+new Sketch({
+  dom: document.getElementById("container"),
+});
