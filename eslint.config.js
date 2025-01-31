ccc461f29843ed219b59c2ad9e04d6a3f9de717f
Keisuke Watanuki <watanuki.keisuke@gmail.com>
2025-01-31 23:34:12 +0900

    Enable eslint and prettier

--- eslint.config.js
+++ eslint.config.js
@@ -2,13 +2,15 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import eslintPluginAstro from 'eslint-plugin-astro';
+import eslintConfigPrettier from "eslint-config-prettier";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.browser }},
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   ...eslintPluginAstro.configs.recommended,
-  ...eslintPluginAstro.configs['jsx-a11y-recommended']
+  ...eslintPluginAstro.configs["jsx-a11y-recommended"],
+  eslintConfigPrettier,
 ];
